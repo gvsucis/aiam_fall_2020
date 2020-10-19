12333b919a87f669761bf2270c967412e39fb490
Max T <toorm@mail.gvsu.edu>
2020-10-19 14:30:50 -0400

    Profile creation formatting, and rescraping already completed profiles

--- aiam/interface/app.py
+++ aiam/interface/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request
 from subprocess import Popen, PIPE
 from os import chdir, listdir
-from json import load, dump
+from json import load, dump, loads
+
+MEMBER_PARAMS_FILENAME = '../member_params.json'
+PROFILES_PATHNAME = '../profiles/'
 
 app = Flask(__name__)
 
@@ -9,6 +12,43 @@ app = Flask(__name__)
 def write_json( data, filename ): 
 	with open(filename,'w') as f: 
 		dump(data, f, indent=4) 
+
+
+def get_profile( filename ):
+	profile = {}
+	with open( filename ) as p:
+		try:
+			profile = load( p )
+			profile = loads( profile )
+		except:
+			return 'err'
+	return profile
+
+
+def clear_memberparams():
+	with open( MEMBER_PARAMS_FILENAME ) as f:
+		params = load( f )
+		params["members"] = {}
+		temp = params["members"]
+		write_json( params, MEMBER_PARAMS_FILENAME )
+
+
+def load_profiles_into_memberparams( profile ):
+	data = {'members':[]}
+	with open( MEMBER_PARAMS_FILENAME ) as f:
+		params = load( f )
+		temp = params["members"]
+		print(profile)
+		temp[ profile.pop('company') ] = profile
+		write_json( params, MEMBER_PARAMS_FILENAME )
+
+
+def scrape():
+	chdir('../')
+	p = Popen( ['scrapy', 'crawl', 'general' ] )
+	p.wait()
+	chdir('interface')
+
       
 @app.route('/process', methods = ['POST'])
 def process():
@@ -19,7 +59,6 @@ def process():
 	print(company_name)
 
 	chdir('../')
-
 	fname = 'member_params.json'
 	with open( fname ) as j: 
 		params = load( j )
@@ -28,18 +67,26 @@ def process():
 		temp = params["members"]
 		temp[company_name] = data
 		write_json( params, fname )
-
 	p = Popen( ['scrapy', 'crawl', 'general' ] )
 	p.wait()
-
-
 	chdir('interface')
-	return 'success'
+	return 'success, new profile created'
+
 
 @app.route('/run_scrapes', methods = ['POST'])
 def run_scrapes():
-	print( request.form );
+	data = request.form
+	data = data.to_dict()
+
+	clear_memberparams()
+
+	for member_profile_file in data:
+		profile = get_profile( PROFILES_PATHNAME + member_profile_file )
+		if type(profile) == dict:
+			load_profiles_into_memberparams( profile )
+	scrape()
 	return 'success'
+
 
 @app.route('/scrape_profiles')
 def scrape_profiles():
@@ -53,7 +100,7 @@ def scrape_profiles():
 
 @app.route('/')
 def hello_world():
-	return render_template('interface.html', scrape_profile_api='http://0.0.0.0:9001/scrape_profiles', profiles=['a','b','c'])
+	return render_template('interface.html', scrape_profile_api='http://0.0.0.0:9000/scrape_profiles', profiles=['a','b','c'])
 
 if __name__ == "__main__":
-	app.run( '0.0.0.0', 9001 )
+	app.run( '0.0.0.0', 9000 )
--- aiam/spiders/general_spider.py
+++ aiam/spiders/general_spider.py
@@ -1,6 +1,7 @@
 import scrapy
 import json
 from selenium import webdriver
+from os import name
 
 PARAM_FILE = 'member_params.json'
 
@@ -19,6 +20,7 @@ class Spider_General(scrapy.Spider):
         self.jobURLAttr = ''
         self.driver = None
 
+
     def write_profile(self):
         with open('profiles/' + self.company + '-profile.json', 'w') as profilef:
             profile = {}
@@ -27,9 +29,15 @@ class Spider_General(scrapy.Spider):
                     continue
                 else:
                     profile[ key ] = self.__dict__[key]
-            profilef.write( json.dump( profile ) )
+            json.dump( profile, profilef )
+
 
     def start_requests(self):
+
+        target_chrome_driver = './ChromeDrivers/linux_chromedriver'
+        if name == 'nt':
+            target_chrome_driver = './ChromeDrivers/chromedriver.exe'
+
         # parse json file into dictionary
         with open( PARAM_FILE, 'r' ) as f:
             members = json.load( f )[ 'members' ]
@@ -38,14 +46,13 @@ class Spider_General(scrapy.Spider):
             # populate self variables from the current member subdictionary
             self.__dict__ = members[member]
             self.company = member
-            self.driver = webdriver.Chrome(executable_path="chromedriver.exe")
+            self.driver = webdriver.Chrome(executable_path=target_chrome_driver)
             # supply scrapy with the data
             yield scrapy.Request( url=self.careersURL, callback=self.parse )
 
-    def parse(self, response):
-        # formatting like this to work seamlessly with firebase
-        data = { self.company: {} }
 
+    def parse(self, response):
+        data = { self.company: {} }
         self.write_profile()
 
         #self.driver.get( response.url )
@@ -66,32 +73,6 @@ class Spider_General(scrapy.Spider):
             
             for job in jobs:              
                 f.write( job.get() + ' -- ' + 'Local' + '\n' )
-
         f.close()
-        #self.driver.close()
 
-        #jobs = response.xpath(self.jobX+'/text()')
-
-        #loc = response.xpath(self.jobX)
-
-        #urls = ""
-        #if jobURLAttr != "N/A":
-        #    urls = response.xpath(self.jobURLX).attrib[self.jobURLAttr],
-
-        '''
-        for job, location,  in ( response.xpath(self.jobsListX), res:
-        # each job to yield fits this dict pattern
-        new_job_title = xpath(self.jobX).get()
-        '''
-        '''
-        for job in jobs:
-            print(job)
-            new_job_info = {
-                'location': 'Ann Arbor', #jobs.xpath(self.locationX).get(),
-                'jobURL': self.jobURLX #self.baseURL + jobs.xpath(self.jobURLX).attrib[self.jobURLAttr]
-            }
-
-            # add the job to the current list of company jobs
-            data[ self.company ][job.get()] = new_job_info
-        '''
         yield data
